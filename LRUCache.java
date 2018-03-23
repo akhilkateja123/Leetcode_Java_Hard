35a30854645b6458d8afd0043ec186fb8fac0317
akhilkateja123 <akhilkateja@gmail.com>
2018-03-23 00:49:42 -0500

    Update LRUCache.java

--- LRUCache.java
+++ LRUCache.java
@@ -3,38 +3,80 @@ package aws.amazon.practice;
 import java.util.HashMap;
 import java.util.Map;
 
+class Node{
+	Node pre;
+	Node next;
+	int key;
+	int value;
+	
+	Node(int key, int value){
+	   this.key=key;
+	   this.value=value;
+	}
+	
+}
+
 public class LRUCache {
 
-	Map<Integer, Integer[]> cache;
+	Map<Integer, Node> map;
+	int count=0;
+	Node head;
+	Node tail;
 	int capacity;
-	int head = -1;
 
 	public LRUCache(int capacity) {
-		cache = new HashMap<Integer, Integer[]>(capacity);
-		this.capacity = capacity;
+		map= new HashMap<>();
+		head= new Node(0,0);
+		tail= new Node(0,0);
+		head.next=tail;
+		tail.pre= head;
+		head.pre=null;
+		tail.next=null;
+	}
+	
+	public void delete(Node node) {
+		node.pre.next= node.next;
+		node.next.pre=node.pre;
+	}
+	
+	public void addToHead(Node node) {
+		node.next=head.next;
+		node.pre=head;
+		head.next.pre=node;
+		head.next=node;
 	}
 
 	public int get(int key) {
-		if (cache.containsKey(key)) {
-			cache.get(key)[1] = cache.get(key)[1] + 1;
-			if (cache.get(key)[1] < cache.get(head)[1])
-				head = key;
-			return cache.get(key)[0];
+		if(map.get(key)!=null) {
+			Node node= map.get(key);
+			int result= node.value;
+			delete(node);
+			addToHead(node);
+			return result;
 		}
+		
 		return -1;
 	}
 
 	public void put(int key, int value) {
-		if (!cache.containsKey(key)) {
-			if (cache.keySet().size() == this.capacity) {
-				cache.remove(head);
+		if(map.get(key)!=null) {
+			Node node= map.get(key);
+			node.value=value;
+			delete(node);
+			addToHead(node);
+			
+		}else {
+			Node node= new Node(key, value);
+			map.put(key, node);
+			if(count<capacity) { count++; addToHead(node);
+			}else {
+				map.remove(tail.pre.key);
+				delete(tail.pre);
+				addToHead(node);
 			}
-			cache.put(key, new Integer[] { value, 1 });
-		} else {
-           cache.get(key)[0]=value;
-           cache.get(key)[1] = cache.get(key)[1] + 1;
-           
+			
 		}
 	}
 
 }
+
